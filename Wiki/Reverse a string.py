5eeb36bcd23d5a93bc2009e387c6ba834ba1947d
David Estrada <135681618+david7zr@users.noreply.github.com>
2026-09-17 23:00:36 -0700

    Delete Wiki directory

--- Wiki/Reverse a string.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(s: str) -> str:
-    return s[::-1]
-
-def run_reverse_string() -> None:
-    s = input("Enter a string to reverse: ")
-    print("Reversed string:", reverse_string(s))
-
-if __name__ == "__main__":
-    run_reverse_string()
